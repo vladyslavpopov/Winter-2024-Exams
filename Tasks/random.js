cccf22d14b5b42dd37762c3683b5d8389e5af8dd
vladyslavpopov <popovvladislav4891@gmail.com>
2024-01-15 00:25:06 +0200

    Removed unnecessary parts of code

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,12 +1,9 @@
 // Generate random integer value in given range
 
 Random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  
+  max = min;
+  return  
 };
 
 module.exports = Random;
