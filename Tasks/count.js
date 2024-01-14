da5136581b928c052b3eedee7aeb208af090dffe
vladyslavpopov <popovvladislav4891@gmail.com>
2024-01-14 19:44:51 +0200

    removed unnecessary semicolons and spaces

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,14 +4,13 @@
 
 const count = (obj) => {
   let sum = 0;
-  
   for (const key of Object.keys(obj)) {
     const value = obj[key];
 
     if (typeof value === 'number') {
       sum += value;
-    };
-  };
+    }
+  }
 
   return sum;
 };
