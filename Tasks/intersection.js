2906c5ecaf8eadec1651b443b672c9d80d030059
vladyslavpopov <popovvladislav4891@gmail.com>
2024-01-14 21:49:58 +0200

    Removed unnecessary braces

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,24 +2,18 @@
 
 intersection = function intersection(object_1, object_2) {
   first_keys = Object.keys(object_1);
-  {
-  }
+
   for (attribute_name of first_keys) {
+
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
+    
     } else {
-      delete object_1[attribute_name];
-      {
-      }
+      delete object_1[attribute_name];   
     }
-    {
-    }
-  }
-  {
-  }
+  
   return object_1;
+  }
 };
 
 module.exports = intersection;
