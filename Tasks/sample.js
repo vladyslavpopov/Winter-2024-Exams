18bcd3731f4d9c48e2bbbcd70052406a265719f5
vladyslavpopov <popovvladislav4891@gmail.com>
2024-01-14 21:24:23 +0200

    Added const where needed

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,7 +1,7 @@
 // Get one random element from an array
 
 const SAMPLE = (arr) => {
-  index = Math.floor(Math.random() * arr.length);
+  const index = Math.floor(Math.random() * arr.length);
 
   return arr[index];
 };
