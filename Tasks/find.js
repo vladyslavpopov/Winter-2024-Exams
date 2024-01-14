588087db71fc1a5139842c1d6f440a2d5e3ea4b9
vladyslavpopov <popovvladislav4891@gmail.com>
2024-01-14 23:30:56 +0200

    Removed unnecessary parts of code

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,16 +1,13 @@
 // Find key by value
 
 find = (object, ...rest) => {
-  value = rest.pop(1);
-  [];
   for (name in object) {
     if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
+
+      return object;
     }
   }
-  rest.push(5020);
+
   return undefined;
 };
 
